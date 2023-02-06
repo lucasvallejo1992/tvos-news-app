14ad2d402b89ccb55aff0418f53599312b45aa4f
Lucas Vallejo <lucasvallejo92@gmail.com>
2023-02-06 13:58:31 -0300

    feat: add nav bar navigation

--- tvos-news-app/ContentView.swift
+++ tvos-news-app/ContentView.swift
@@ -9,13 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        TabView{
+            Text("News")
+                .tabItem {
+                    Label("News", systemImage: "newspaper")
+                }
+                .tag("news")
+            Text("Saved")
+                .tabItem {
+                    Label("Saved", systemImage: "bookmark")
+                }
+                .tag("saved")
+            Text("Search")
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                }
+                .tag("search")
         }
-        .padding()
     }
 }
 
